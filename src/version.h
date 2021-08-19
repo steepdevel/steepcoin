706a2080467824ae02bd7eb428e5fe4091c0597d
steepdevel <78454106+steepdevel@users.noreply.github.com>
2021-08-19 08:11:31 +0300

    Update version.h

--- src/version.h
+++ src/version.h
@@ -9,7 +9,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 85023;
+static const int PROTOCOL_VERSION = 70015;
 static const int OLD_VERSION = 85023; // steepcoin: used to communicate with clients that don't know how to send PoS information in headers
 
 //! initial proto version, to be increased after version/verack negotiation
@@ -29,7 +29,7 @@ static const int CADDR_TIME_VERSION = 31402;
 static const int BIP0031_VERSION = 60000;
 
 //! "filter*" commands are disabled without NODE_BLOOM after and including this version
-static const int NO_BLOOM_VERSION = 70011;
+static const int NO_BLOOM_VERSION = 85023;
 
 //! "sendheaders" command and announcing blocks with headers starts with this version
 static const int SENDHEADERS_VERSION = 70012;
