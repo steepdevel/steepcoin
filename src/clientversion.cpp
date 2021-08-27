0bffb883ab60fcf834fc48717429e191f00535d6
steepdevel <78454106+steepdevel@users.noreply.github.com>
2021-08-27 07:07:25 +0300

    Update clientversion.cpp

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -12,7 +12,7 @@
  * for both steepcoind and steepcoin-qt, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-// const std::string CLIENT_NAME("Satoshi");
+ const std::string CLIENT_NAME("SteepCoin");
 
 /**
  * Client version number
