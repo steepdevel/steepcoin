d16e71d3fa6d67dd904c749a2735f5a877673792
vivisko <vivisko68@gmail.com>
2021-09-17 04:00:58 +0300

    Fix170921

--- src/bloom.h
+++ src/bloom.h
@@ -9,6 +9,9 @@
 
 #include <vector>
 
+#include "uint256.h"
+#include "serialize.h"
+
 class COutPoint;
 class CTransaction;
 class uint256;
@@ -17,10 +20,8 @@ class uint256;
 static const unsigned int MAX_BLOOM_FILTER_SIZE = 36000; // bytes
 static const unsigned int MAX_HASH_FUNCS = 50;
 
-/**
- * First two bits of nFlags control how much IsRelevantAndUpdate actually updates
- * The remaining bits are reserved
- */
+// First two bits of nFlags control how much IsRelevantAndUpdate actually updates
+// The remaining bits are reserved
 enum bloomflags
 {
     BLOOM_UPDATE_NONE = 0,
@@ -58,17 +59,15 @@ private:
     friend class CRollingBloomFilter;
 
 public:
-    /**
-     * Creates a new bloom filter which will provide the given fp rate when filled with the given number of elements
-     * Note that if the given parameters will result in a filter outside the bounds of the protocol limits,
-     * the filter created will be as close to the given parameters as possible within the protocol limits.
-     * This will apply if nFPRate is very low or nElements is unreasonably high.
-     * nTweak is a constant which is added to the seed value passed to the hash function
-     * It should generally always be a random value (and is largely only exposed for unit testing)
-     * nFlags should be one of the BLOOM_UPDATE_* enums (not _MASK)
-     */
-    CBloomFilter(const unsigned int nElements, const double nFPRate, const unsigned int nTweak, unsigned char nFlagsIn);
-    CBloomFilter() : isFull(true), isEmpty(false), nHashFuncs(0), nTweak(0), nFlags(0) {}
+    // Creates a new bloom filter which will provide the given fp rate when filled with the given number of elements
+    // Note that if the given parameters will result in a filter outside the bounds of the protocol limits,
+    // the filter created will be as close to the given parameters as possible within the protocol limits.
+    // This will apply if nFPRate is very low or nElements is unreasonably high.
+    // nTweak is a constant which is added to the seed value passed to the hash function
+    // It should generally always be a random value (and is largely only exposed for unit testing)
+    // nFlags should be one of the BLOOM_UPDATE_* enums (not _MASK)
+    CBloomFilter(unsigned int nElements, double nFPRate, unsigned int nTweak, unsigned char nFlagsIn);
+    CBloomFilter() : isFull(true) {}
 
     ADD_SERIALIZE_METHODS;
 
